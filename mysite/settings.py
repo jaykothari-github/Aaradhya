68e4ef92afb98e086bf9c40787bf299af3ec194c
Jay Kothari <jaykothari49@gmail.com>
2025-03-20 00:08:34 +0530

    bug fix in js

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,5 +133,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "kotharijay.tops@gmail.com"
-EMAIL_HOST_PASSWORD = "whevdvsvdffltlsb"
+EMAIL_HOST_USER = "aaradhya.garba.group@gmail.com"
+EMAIL_HOST_PASSWORD = "qlrkorysyfwgslrf"
