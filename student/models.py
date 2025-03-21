745927da530037b22b310cddb3047691e072619d
Jay Kothari <jaykothari49@gmail.com>
2025-03-22 03:35:38 +0530

    model finalise

--- student/models.py
+++ student/models.py
@@ -24,8 +24,16 @@ class Student(models.Model):
     verified = models.BooleanField(default=False)
     aadhar = models.CharField(max_length=12)
     aadhar_image = models.FileField(upload_to='aadhar/', blank=True, null=True)
+    aadhar_verified = models.BooleanField(default=False)
     profile_qr = models.FileField(upload_to='profile_qr/',default='default/qr.png')
     profile_image = models.FileField(upload_to='profile/', default='default/profile.jpg')
+    profile_image_verified = models.BooleanField(default=False)
+    fees_amount = models.IntegerField(default=0)
+    fees_paid = models.IntegerField(default=0)
+    fees_status = models.BooleanField(default=False)
+    batch_name = models.CharField(max_length=30, blank=True, null=True)
+    batch_start_date = models.DateField(blank=True, null=True)
+    batch_end_date = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
