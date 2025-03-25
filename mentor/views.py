452af4f7063697cc94d212ffd44933e80ffe4ba5
Jay Kothari <jaykothari49@gmail.com>
2025-03-25 23:27:37 +0530

    Mentor Home complete

--- mentor/views.py
+++ mentor/views.py
@@ -6,6 +6,20 @@ from django.shortcuts import redirect
 # Create your views here.
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        try:
+            student = Student.objects.get(email=email)
+            if student.password == password:
+                if student.role == 'Student':
+                    return render(request, 'login.html', {'msg': 'You are not authorized to access this page'})
+                request.session['email'] = email
+                return redirect('mentor-index')
+            else:
+                return render(request, 'login.html', {'msg': 'Invalid credentials'})
+        except:
+            return render(request, 'login.html', {'msg': 'Invalid credentials'})
     return render(request, 'login.html')
 
 def index(request):
@@ -17,15 +31,17 @@ def index(request):
         students = Student.objects.all()
         students_count = students.count() 
         fees_pending = students.filter(fees_status=False).count()
-        fees_paid = students.filter(fees_status=True, verified=True, password_reset=True, profile_image_verified=True, aadhar_verified=True ).count() 
+        pending_verification = students.filter( Q(profile_image_verified=False) | Q(aadhar_verified=False)).exclude(verified=False).order_by('created_at')[:8]
+        all_verified = students.filter(fees_status=True, verified=True, password_reset=True, profile_image_verified=True, aadhar_verified=True ).count() 
         unverified_list = students.filter(Q(verified=False) | Q(password_reset=False)).order_by('created_at')
         aadhar_unverified = students.filter(aadhar_verified=False).count()
         profile_unverified = students.filter(profile_image_verified=False).count()
         return render(request, 'index.html', {'students_count': students_count, 
                                               'unverified_list': unverified_list,
+                                              'pending_verification': pending_verification,
                                               'student': student, 
                                               'fees_pending': fees_pending, 
-                                              'fees_paid': fees_paid, 
+                                              'all_verified': all_verified, 
                                               'aadhar_unverified': aadhar_unverified, 
                                               'profile_unverified': profile_unverified})
     # except:
