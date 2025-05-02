03be094b097e596e29b6f6514375c518054cff52
Jay Kothari <jaykothari49@gmail.com>
2025-05-03 03:09:33 +0530

    admin action for unblock id cards added

--- student/admin.py
+++ student/admin.py
@@ -7,14 +7,18 @@ from .models import *
 # class ModelMember(admin.ModelAdmin):
 #     list_display = ['first_name', 'last_name', 'id', 'email','aadhar','verified']
 
-@admin.action(description="block ID cards")
+@admin.action(description="Block ID cards")
 def block_id(modeladmin, request, queryset):
     queryset.update(block=True)
+
+@admin.action(description="Unblock ID cards")
+def unblock_id(modeladmin, request, queryset):
+    queryset.update(block=False)
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'id', 'email','aadhar','verified']
     list_filter = ['block']
     search_fields = ['first_name', 'last_name', 'email','aadhar']
-    actions = [block_id]
+    actions = [block_id, unblock_id]
 
 admin.site.register(Student, StudentAdmin)
